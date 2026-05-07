1715867c738f79ce6c34897a2ca3c935e2114c43
Dimss_spa <dimasatriapa@gmail.com>
2026-05-07 14:35:22 +0700

    menambahkan halaman tentang saya

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/about',function(){
 Route::get('/contact',function(){
     return view('contact');
 });
+
+Route::get('/tentang_saya',function(){
+    return view('tentang_saya');
+});
