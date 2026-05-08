e08e5351a174ce282e8f2983553874f567306e56
Dimss_spa <dimasatriapa@gmail.com>
2026-05-08 15:20:49 +0700

    Halaman register (#11)
    * halaman regis
    
    * menambahkan halaman profil
    
    * nambah profile
    
    * menambahkan halaman tentang saya
    
    * mengubah code tentang_saya
    
    * typo pada nama file tentang_saya
    
    * menambahkan halaman tentang saya
    
    * mengubaha halaman register
    
    * sedikit iseng
    
    * merubah code
    
    ---------
    
    Co-authored-by: Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
    Co-authored-by: Nalvenzon <Wimpywcw11@gmail.com>

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,11 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+ <link rel="stylesheet" href="/catatan/public/css/style.css">
 </head>
 <body>
-    <h2>Silahkan registrasi terlebih dulu</h2>
-    
+    <h1>Perubahan Baru</h1>
+    <h2>merubah co</h2>
+    <div class="container">
+        <div class="card">
+            <div class="card-headrer">Register Admin Baru</div>
+            <?php if(isset($error)) echo"<div class='alert alert-danger'>$error</div>";?>
+            <?php if(isset($success)) echo"<div class='alert alert-success'>$success</div>";?>
+            <form action="../catatan/index.php?act=register-process" method="POST">
+                <div class="form-group">
+                    <label>Username:</label>
+                    <input type="text" name="username" class="form-control" required>
+                </div>
+                <div class="form=group">
+                    <label>Password:</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                 <button type="submit" class="btn">Daftar</button>
+            </form>
+            <hr>
+            <a href="/catatan/index.php?act=login" class="text-center">Sudah punya akun?</a>
+                <a href="login.blade.php">Login</a>
+    </div>
 </body>
 </html>
