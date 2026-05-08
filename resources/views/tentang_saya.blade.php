76713715719fad82ecfbb766eba0ead6d790c396
Dimss_spa <dimasatriapa@gmail.com>
2026-05-08 09:13:02 +0700

    menambahkan halaman tentang saya

---
+++ resources/views/tentang_saya.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ <link rel="stylesheet" href="/catatan/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-headrer">Register Admin Baru</div>
+            <?php if(isset($error)) echo"<div class='alert alert-danger'>$error</div>";?>
+            <?php if(isset($success)) echo"<div class='alert alert-success'>$success</div>";?>
+            <form action="../catatan/index.php?act=register-process" method="POST">
+                <div class="form-group">
+                    <label>Username:</label>
+                    <input type="text" name="username" class="form-control" required>
+                </div>
+                <div class="form=group">
+                    <label>Password:</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                 <button type="submit" class="btn">Daftar</button>
+            </form>
+            <hr>
+            <a href="/catatan/index.php?act=login" class="text-center">Sudah punya akun?</a>
+    </div>
+</body>
+</html>
+</body>
+</html>
